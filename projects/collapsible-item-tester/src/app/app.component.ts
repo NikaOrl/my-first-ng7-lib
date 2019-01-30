ad6bff76b869340a31386df1af4670acf8d046e5
Nika Orlova <Veronika_Orlova@epam.com>
2019-01-30 22:02:05 +0300

    component styles and html changed

--- projects/collapsible-item-tester/src/app/app.component.ts
+++ projects/collapsible-item-tester/src/app/app.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'collapsible-item-tester';
-  public items: Array<string> = ['a', 'b', 'c'];
+  items: Array<any> = [
+    {
+      id: 1,
+      name: 'Vasilii Ivanov',
+      age: 18,
+      country: 'Russia',
+      score: '50%'
+  }, {
+      id: 2,
+      name: 'Ivan Vasilev',
+      age: 20,
+      country: 'Russia',
+      score: '70%'
+  }, {
+      id: 3,
+      name: 'Petr Petrov',
+      age: 21,
+      country: 'Russia',
+      score: '90%'
+  }];
 }
--- projects/nx-collapsible-item/src/lib/components/collapsible-item/nx-collapsible-item.component.ts
+++ projects/nx-collapsible-item/src/lib/components/collapsible-item/nx-collapsible-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'enl-nx-collapsible-item',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['nx-collapsible-item.component.css']
 })
 export class NxCollapsibleItemComponent implements OnInit {
-  public itemId: number = 0;
+  @Input() item: any;
   public isCollapsed = false;
 
   constructor() {}
